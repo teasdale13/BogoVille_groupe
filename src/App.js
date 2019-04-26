ddaf20d2193b254c699c70f0a5a38bed6e92f5df
Kevin Teasdale-Dubé <kevin_teasdale_dube@hotmail.com>
2019-04-25 21:01:57 -0400

    comment code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import KillerApp from './KillerApp';
 
 
 export class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            test: ""
+            test: "",
+            model:""
+        };
+        this.Child = this.Child.bind(this);
+    }
+
+    /**
+     * Requete http via Axios au BackEnd (index.php) du site internet
+     * pour avoir l'information du Rest API
+     */
+    onCharge (model, id){
+        const axios = require('axios');
+        let data = "";
+        axios.get('/' + model.toString() /* + "/" + id.toString()*/)
+            .then(function (response) {
+                console.log(response.data);
+                data = response.data.toString();
+                this.setState({test: data});
+            }.bind(this))
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+    }
+
+    Child({match}) {
+        console.log(this);
+        /* Vérification pour éviter les requêtes en boucle infinie */
+        if (match.params.model !== this.state.model){
+            this.onCharge(match.params.model, match.params.id );
+            this.setState({model: match.params.model})
         }
-    }
-
-    componentWillMount() {
-       this.onCharge();
-    }
-
-    onCharge(){
-      const axios = require('axios');
-      let data = "";
-      axios.get('/test')
-          .then(function (response) {
-              console.log(response.data);
-              data = response.data.toString();
-              this.setState({test: data});
-          }.bind(this))
-          .catch(function (error) {
-              // handle error
-              console.log(error);
-          }).then(function () {
-          // always executed
-
-      })
-  }
-
-    Child({ match }) {
-        return (
+        const test =
             <div>
                 <h1>MODEL: {match.params.model}</h1>
+                <h1>ID: {match.params.id}</h1>
+                <h3>URL: {this.state.test}</h3>
+            </div>;
+
+        return (
+            <div>
+                {test}
             </div>
         );
-    }
 
+    }
 
   render() {
     return (
@@ -50,14 +61,15 @@ export class App extends React.Component {
                 <p>{this.state.test}</p>
                 <ul>
                     <li>
-                        <Link to="/evenement">Evenement</Link>
+                        <Link to="/evenement/2">Evenement</Link>
                     </li>
                     <li>
-                        <Link to="/probleme">Probleme</Link>
+                        <Link to="/probleme/4">Probleme</Link>
                     </li>
                 </ul>
 
-                <Route path="/:model" component={this.Child}/>
+                /* :model représente evenement et problème des tag <Link> et :id 2 et 4 */
+                <Route path="/:model/:id" component={this.Child}/>
 
             </div>
         </Router>
